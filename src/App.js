49a13ba46e378d4f94d406c99e206e1cdb86b76d
DJ Asuncion <deorabbinjibe777@gmail.com>
2020-02-24 22:11:24 +0800

    29/29!!!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-import {
-  useTimeLeftShifter,
-  useCountdownTimer,
-  useShifter,
-  useAbsoluteZero,
-  usePhaseToggler,
-  useAlarm,
-  usePause
-} from "./Hooks";
+import {} from "./Hooks";
 
 import { GlobalStyles } from "./GlobalStyles";
 import {
@@ -23,45 +15,52 @@ import {
   Audio
 } from "./components";
 
+let timerID;
+
 function App() {
   const DEFAULT_SESSION_LENGTH = 1500;
   const DEFAULT_BREAK_LENGTH = 300;
   const audioRef = useRef();
 
   const [phase, setPhase] = useState(true);
+  useEffect(() => {
+    if (phase) {
+      setTimeLeft(sessionLength);
+    } else {
+      setTimeLeft(breakLength);
+    }
+  }, [phase]);
 
   const [play, setPlay] = useState(false);
 
   const [pause, setPause] = useState(false);
 
   const [shifting, setShifting] = useState(false);
+  useEffect(() => {
+    if (shifting) {
+      const timeoutID = setTimeout(() => {
+        setPhase(!phase);
+        setShifting(false);
+        startTime();
+      }, 1000);
+
+      return () => clearTimeout(timeoutID);
+    }
+  });
 
   const [sessionLength, setSessionLength] = useState(DEFAULT_SESSION_LENGTH);
 
   const [breakLength, setBreakLength] = useState(DEFAULT_BREAK_LENGTH);
 
   const [timeLeft, setTimeLeft] = useState(DEFAULT_SESSION_LENGTH);
+  useEffect(() => {
+    if (timeLeft === 0) {
+      stopTime();
+      audioRef.current.play();
+      setShifting(true);
+    }
+  }, [timeLeft]);
 
-  const isSession = usePhaseToggler(play, phase, shifting);
-
-  useCountdownTimer(play, setTimeLeft);
-  useTimeLeftShifter(play, phase, setTimeLeft, sessionLength, breakLength);
-
-  useShifter(play, timeLeft, setShifting);
-  useAbsoluteZero(
-    shifting,
-    setShifting,
-    setPhase,
-    phase,
-    setTimeLeft,
-    sessionLength,
-    breakLength,
-    play,
-    isSession
-  );
-
-  useAlarm(audioRef, timeLeft);
-  usePause(pause);
   const handleReset = () => {
     setPhase(true);
     setPlay(false);
@@ -72,6 +71,67 @@ function App() {
     setTimeLeft(DEFAULT_SESSION_LENGTH);
     audioRef.current.pause();
     audioRef.current.currentTime = 0;
+    clearInterval(timerID);
+  };
+
+  const startTime = () => {
+    timerID = setInterval(() => {
+      setTimeLeft(tL => tL - 1);
+    }, 1000);
+  };
+
+  const stopTime = () => {
+    clearInterval(timerID);
+  };
+
+  const handleClick = e => {
+    const ID = e.target.id;
+
+    switch (ID) {
+      case "break-increment":
+        if (breakLength === 60 * 60) return;
+        setBreakLength(breakLength + 60);
+        break;
+      case "break-decrement":
+        if (breakLength < 120) return;
+        setBreakLength(breakLength - 60);
+        break;
+      case "session-increment":
+        if (sessionLength === 60 * 60) return;
+        if (!play) {
+          setTimeLeft(sessionLength + 60);
+        }
+        setSessionLength(sessionLength + 60);
+        break;
+      case "session-decrement":
+        if (sessionLength < 120) return;
+        if (!play) {
+          setTimeLeft(sessionLength - 60);
+        }
+        setSessionLength(sessionLength - 60);
+        break;
+      case "reset":
+        handleReset();
+        break;
+      case "start_stop":
+        if (!play) {
+          setPlay(true);
+          startTime();
+        } else if (play && !pause) {
+          setPause(true);
+          stopTime();
+        } else {
+          setPause(false);
+          startTime();
+        }
+        break;
+      case "pause":
+        if (!play || pause) return;
+        setPause(true);
+        break;
+      default:
+        return;
+    }
   };
 
   console.log(`
@@ -82,9 +142,8 @@ function App() {
   sessionLength = ${sessionLength}
   breakLength = ${breakLength}
   timeLeft = ${timeLeft}
-  isSession = ${isSession}
   audioRef = ${audioRef.current}
-  
+  timerID = ${timerID}
   `);
 
   console.log(audioRef);
@@ -96,36 +155,20 @@ function App() {
         <Title>Pomodoro Clock</Title>
         <LengthControlsWrapper>
           <LengthControls id="break">
-            <Button
-              id="break-decrement"
-              breakLength={breakLength}
-              setBreakLength={setBreakLength}
-            >
+            <Button id="break-decrement" handleClick={handleClick}>
               down
             </Button>
             <span id="break-length">{Math.floor(breakLength / 60)}</span>
-            <Button
-              id="break-increment"
-              breakLength={breakLength}
-              setBreakLength={setBreakLength}
-            >
-              up
+            <Button id="break-increment" handleClick={handleClick}>
+              &#11014;
             </Button>
           </LengthControls>
           <LengthControls id="session" length={sessionLength}>
-            <Button
-              id="session-decrement"
-              sessionLength={sessionLength}
-              setSessionLength={setSessionLength}
-            >
+            <Button id="session-decrement" handleClick={handleClick}>
               down
             </Button>
             <span id="session-length">{Math.floor(sessionLength / 60)}</span>
-            <Button
-              id="session-increment"
-              sessionLength={sessionLength}
-              setSessionLength={setSessionLength}
-            >
+            <Button id="session-increment" handleClick={handleClick}>
               up
             </Button>
           </LengthControls>
@@ -133,19 +176,13 @@ function App() {
         <TimerWrapper play={play} phase={phase} timeLeft={timeLeft} />
 
         <BottomControlsWrapper>
-          <Button
-            id="start_stop"
-            setPlay={setPlay}
-            pause={pause}
-            setPause={setPause}
-            play={play}
-          >
+          <Button id="start_stop" handleClick={handleClick}>
             start/stop
           </Button>
-          <Button id="pause" pause={pause} setPause={setPause} play={play}>
+          <Button id="pause" handleClick={handleClick}>
             pause
           </Button>
-          <Button id="reset" handleReset={handleReset}>
+          <Button id="reset" handleClick={handleClick}>
             reset
           </Button>
         </BottomControlsWrapper>
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,59 +2,10 @@ import React from "react";
 
 import { StyledButton } from "./Button.styled";
 
-const Button = ({
-  children,
-  id,
-  setPlay,
-  pause,
-  setPause,
-  play,
-  handleReset,
-  breakLength,
-  setBreakLength,
-  sessionLength,
-  setSessionLength
-}) => {
-  const handleClick = e => {
-    switch (id) {
-      case "break-increment":
-        if (breakLength === 60 * 60) return;
-        setBreakLength(breakLength + 60);
-        break;
-      case "break-decrement":
-        if (breakLength < 120) return;
-        setBreakLength(breakLength - 60);
-        break;
-      case "session-increment":
-        if (sessionLength === 60 * 60) return;
-        setSessionLength(sessionLength + 60);
-        break;
-      case "session-decrement":
-        if (sessionLength < 120) return;
-        setSessionLength(sessionLength - 60);
-        break;
-      case "reset":
-        handleReset();
-        break;
-      case "start_stop":
-        if (!play) {
-          setPlay(true);
-        } else {
-          setPause(!pause);
-        }
-        break;
-      case "pause":
-        if (!play || pause) return;
-        setPause(true);
-        break;
-      default:
-        return;
-    }
-  };
-
+const Button = ({ children, id, handleClick }) => {
   return (
     <StyledButton id={id} onClick={handleClick}>
-      <span>{children}</span>
+      {children}
     </StyledButton>
   );
 };
